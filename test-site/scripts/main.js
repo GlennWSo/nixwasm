5e55d6ba965d3983baba462b75d7813741fc9bde
GlennWSo <sondellglenn@gmail.com>
2023-01-26 00:50:46 +0100

    added name btn

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -11,3 +11,28 @@ myImage.onclick = () => {
     myImage.setAttribute("src", img1);
   }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+
+function setUserName() {
+  let myName = prompt("Please enter your name.");
+  if (!myName) {
+    return 
+  }
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Hello, ${myName}!`;
+}
+
+
+myButton.onclick = () => {
+  setUserName();
+};
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Hello, ${storedName}`
+}
